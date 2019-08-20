7a773a0a15aa8f985ac3011930e9c528e9033fd1
AKAPangHu <352032370@qq.com>
2019-08-20 19:16:57 +0800

    init

--- src/No4.java
+++ src/No4.java
@@ -4,4 +4,23 @@
  **/
 public class No4 {
 
+    public boolean Find(int target, int [][] array) {
+        int rows = array.length;
+        int cols = array[0].length;
+        int col = cols - 1;
+        int row = 0;
+        while (row < rows && col > 0){
+            if (target == array[row][col]){
+                return true;
+            }
+            else if (target < array[row][col]){
+                col--;
+            }
+            else{
+                row++;
+            }
+        }
+        return false;
+    }
+
 }
